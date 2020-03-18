29c63c0d647cbc6585c059434e0ec30b0bf39b4f
manhph43k14 <manhph43k14@users.noreply.github.com>
2020-03-19 00:40:44 +0700

    Create Title.java

---
+++ Smart control device/App/app/src/main/java/com/example/xoaphm/smartdevicecontrol/models/Title.java
@@ -0,0 +1,15 @@
+package com.example.xoaphm.smartdevicecontrol.models;
+
+
+
+public class Title {
+    private String name;
+
+    public Title(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
